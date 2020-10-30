6b15bdb607f51deb2056b52d28d94083343e244a
Adem TEN <65685868+AdemTEN@users.noreply.github.com>
2020-10-30 13:13:50 +0100

    Update CukesRunner.java

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
-        dryRun = fal,
+        dryRun = fals,
         tags = "@smoke"
 )
 
